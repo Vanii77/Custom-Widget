90ea49816e012672c97306810bcd2389c073a81b
Vanii77 <127956785+Vanii77@users.noreply.github.com>
2023-03-15 16:02:27 +0100

    Update Linechart.js

--- Linechart.js
+++ Linechart.js
@@ -1,24 +1,76 @@
-import * as echarts from 'echarts';
-import { parseMetadata } from '../utils/data-binding/parse';
+import * as echarts from 'echarts'
+import html from './kpi_ring.html'
 
-var chartDom = document.getElementById('main');
-var myChart = echarts.init(chartDom);
-var option;
+import { parseMetadata } from '../utils/data-binding/parse'
 
-option = {
-  xAxis: {
-    type: 'category',
-    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-  },
-  yAxis: {
-    type: 'value'
-  },
-  series: [
-    {
-      data: [150, 230, 224, 218, 135, 147, 260],
-      type: 'line'
+(function () {
+  const template = document.createElement('template')
+  template.innerHTML = html
+  class SampleRingKpi extends HTMLElement {
+    constructor () {
+      super()
+
+      this._shadowRoot = this.attachShadow({ mode: 'open' })
+      this._shadowRoot.appendChild(template.content.cloneNode(true))
+
+      this._root = this._shadowRoot.getElementById('root')
+
+      this._props = {}
+
+      this._echart = undefined
+      this.render()
     }
-  ]
-};
+
+    onCustomWidgetResize (width, height) {
+      this.render()
+    }
+
+    set myDataSource (dataBinding) {
+      this._myDataSource = dataBinding
+      this.render()
+    }
+
+    async render () {
+      this.dispose()
+
+      if (!this._myDataSource || this._myDataSource.state !== 'success') {
+        return
+      }
+
+      const { data, metadata } = this._myDataSource
+      const { dimensions, measures } = parseMetadata(metadata)
+
+      this._echart = echarts.init(this._root, 'wight')
+
+      const option = {
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+      yAxis: {
+        type: 'value'
+        },
+      series: [
+        {
+        data: [150, 230, 224, 218, 135, 147, 260],
+        type: 'line'
+         }
+      ]
+    };
+      this._echart.setOption(option)
+    }
+
+    dispose () {
+      if (this._echart) {
+        echarts.dispose(this._echart)
+      }
+    }
+  }
+
+  customElements.define('com-sap-sample-echarts-ring_kpi', SampleRingKpi)
+})()
+
+
+
 
 option && myChart.setOption(option);
