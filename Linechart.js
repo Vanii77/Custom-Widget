67bbcf9da63c15e5e6a0cafd2eda51b4dff3e8b9
Vanii77 <127956785+Vanii77@users.noreply.github.com>
2023-03-15 15:56:31 +0100

    Update Linechart.js

--- Linechart.js
+++ Linechart.js
@@ -1,70 +1,24 @@
-import * as echarts from 'echarts'
-import html from './GrogressGauge.html'
+import * as echarts from 'echarts';
+import { parseMetadata } from '../utils/data-binding/parse';
 
-import { parseMetadata } from '../utils/data-binding/parse'
+var chartDom = document.getElementById('main');
+var myChart = echarts.init(chartDom);
+var option;
 
-(function () {
-  const template = document.createElement('template')
-  template.innerHTML = html
-  class GrogressGauge extends HTMLElement {
-    constructor () {
-      super()
-
-      this._shadowRoot = this.attachShadow({ mode: 'open' })
-      this._shadowRoot.appendChild(template.content.cloneNode(true))
-
-      this._root = this._shadowRoot.getElementById('root')
-
-      this._props = {}
-
-      this._echart = undefined
-      this.render()
+option = {
+  xAxis: {
+    type: 'category',
+    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+  },
+  yAxis: {
+    type: 'value'
+  },
+  series: [
+    {
+      data: [150, 230, 224, 218, 135, 147, 260],
+      type: 'line'
     }
+  ]
+};
 
-    onCustomWidgetResize (width, height) {
-      this.render()
-    }
-
-    set myDataSource (dataBinding) {
-      this._myDataSource = dataBinding
-      this.render()
-    }
-
-    async render () {
-      this.dispose()
-
-      if (!this._myDataSource || this._myDataSource.state !== 'success') {
-        return
-      }
-
-      const { data, metadata } = this._myDataSource
-      const { dimensions, measures } = parseMetadata(metadata)
-
-      this._echart = echarts.init(this._root, 'wight')
-      const option = {
-        xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-         },
-        yAxis: {
-        type: 'value'
-          },
-        series: [
-         {
-            data: [150, 230, 224, 218, 135, 147, 260],
-             type: 'line'
-         }
-      ]
-    };
-      this._echart.setOption(option)
-    }
-
-    dispose () {
-      if (this._echart) {
-        echarts.dispose(this._echart)
-      }
-    }
-  }
-
-  customElements.define('com-sap-sample-echarts-grogress_gauge', GrogressGauge)
-})()
+option && myChart.setOption(option);
