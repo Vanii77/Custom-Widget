4338738f640a64b5e848d138b134e23d8e0ae5c8
Vanii77 <127956785+Vanii77@users.noreply.github.com>
2023-03-22 16:04:14 +0100

    Update Linechart.js

--- Linechart.js
+++ Linechart.js
@@ -45,14 +45,15 @@ var getScriptPromisify = (src) => {
       const option = {
         xAxis: {
             type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            value: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
             },
         yAxis: {
             type: 'value'
             },
         series: [
             {
-            data: [150, 230, 224, 218, 135, 147, 260] 
+            name: "",
+            value: [150, 230, 224, 218, 135, 147, 260], 
             type: 'line'
         },
     ],
